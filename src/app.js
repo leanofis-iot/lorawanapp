1b72c58772126be8bd8b9f0b2b944d3bb1ade88f
emitterpark <42601356+emitterpark@users.noreply.github.com>
2019-07-10 20:52:57 +0300

    init

--- src/app.js
+++ src/app.js
@@ -1,49 +1,99 @@
-const {Button, TextInput, TextView, ui} = require('tabris');
+const {Button, TextInput, TextView, Composite, CollectionView, ui} = require('tabris')
+
+console.log(screen.width)
+console.log(screen.height)
+
+let config = [
+  ['dev_eui', 'XXXXXXXXXXXXXXXX'],
+  ['app_eui', 'XXXXXXXXXXXXXXXX'],
+  ['app_key', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'],
+  ['pow_tm', 'XXXXXXXXXXXXXXXX'],
+  ['bat_lo_v', 'XXXXXXXXXXXXXXXX'],
+  ['interface', 'XXXXXXXXXXXXXXXX'],
+  ['sensor', 'XXXXXXXXXXXXXXXX'],
+  ['ntc_tmp', 'XXXXXXXXXXXXXXXX'],
+  ['ntc_res', 'XXXXXXXXXXXXXXXX'],
+  ['ntc_beta', 'XXXXXXXXXXXXXXXX'],
+  ['ntc_ser_res', 'XXXXXXXXXXXXXXXX'],
+  ['an_num_samp', 'XXXXXXXXXXXXXXXX'],
+  ['an_dly_samp', 'XXXXXXXXXXXXXXXX'],
+  ['an_alr_en', 'XXXXXXXXXXXXXXXX'],
+  ['an_alr_hi_set', 'XXXXXXXXXXXXXXXX'],
+  ['an_alr_hi_clr', 'XXXXXXXXXXXXXXXX'],
+  ['an_alr_lo_set', 'XXXXXXXXXXXXXXXX'],
+  ['an_alr_lo_clr', 'XXXXXXXXXXXXXXXX'],
+  ['dig_alr_hi_en', 'XXXXXXXXXXXXXXXX'],
+  ['dig_alr_lo_en', 'XXXXXXXXXXXXXXXX']
+  ].map(([name, value]) => ({name, value}))
 
 let open = false
 
+new CollectionView({
+  left: 0, top: 80, right: 0, height: 250, background: 'red',
+  itemCount: config.length,
+  cellHeight: 120,
+  createCell: () => {
+    let cell = new Composite()
+    new TextView({
+      centerX: 0, top: 'prev() 30', alignment: 'center', font: 'bold 24px'
+    }).appendTo(cell)
+    new TextInput({
+      centerX: 0, top: 'prev() 30', alignment: 'center'
+    }).appendTo(cell)
+    return cell
+  },
+  updateCell: (cell, index) => {
+    let param = config[index]
+    cell.apply({
+        TextView: {text: param.name},
+        TextInput: {text: param.value}
+    })
+  }
+}).on('select', ({index}) => console.log('selected', config[index].name))
+  .appendTo(ui.contentView)
+
+let connect = new Button({
+    top: 20, left: 40, width: 80, text: 'Connect'   
+}).appendTo(ui.contentView)
+let receive = new Button({
+    top: 20, left: 140, width: 80, text: 'Receive'   
+}).appendTo(ui.contentView)
 let send = new Button({
-    top: 100, left:100   
+    top: 20, left: 240, width: 80, text: 'Send'   
 }).appendTo(ui.contentView)
 
+
+/*
 let tx_data = new TextInput({
-    top: 150, left:100
+    top: 150, left:100, text: 'at+version', background: 'yellow'
 }).appendTo(ui.contentView)
 
 let  rx_data = new TextView({
-    top: 200, left:100
+    top: 200, left:100, text: 'hello', background: 'red'
 }).appendTo(ui.contentView)
 
 function errorCallback (message) {
-    new AlertDialog({
-        title: 'Error',
-        message: message,
-        buttons: {ok: 'OK'}
-    }).open() 
+    console.log(message)
 }
 
 serial.requestPermission(
+    {driver: 'Ch34xSerialDriver'},
     function(successMessage) {
     	serial.open(
-        	{baudRate: 9600},
+        	{baudRate: 115200},
             function(successMessage) {
                 open = true                
-                new AlertDialog({
-                    title: 'Port Open',
-                    message: successMessage,
-                    buttons: {ok: 'OK'}
-                }).open()
+                console.log(successMessage)
+                console.log('ready')
                 serial.registerReadCallback(
-                    function success(data){
-                        let view = new Uint8Array(data)
-                        rx_data.text = view
+                    function success(data){  
+                        let view = new Uint8Array(data);
+                        console.log(view); 
+                        console.log(String.fromCharCode.apply(null, view))                     
+                        console.log('callback works')
                     },
                     function error(){
-                        new AlertDialog({
-                            title: 'Error',
-                            message: "Failed to register read callback",
-                            buttons: {ok: 'OK'}
-                        }).open()
+                        console.log("Failed to register read callback")
                     } 
                 )
 
@@ -54,6 +104,9 @@ serial.requestPermission(
     errorCallback
 )
 send.on('select', () => {
-    console.log('click')
-    if (open) serial.write(tx_data.text + '\r\n');
+    console.log('send it')       
+    //let str = 'at+join=otaa' + String.fromCharCode(13, 10)
+    let str = 'at+send=0,1,01670110' + '\r\n'
+    serial.write(str)     
 })
+*/
