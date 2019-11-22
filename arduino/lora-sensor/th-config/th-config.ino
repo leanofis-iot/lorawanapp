508d3d7644a463f1d3ca1a123082fefa458849f4
emitterpark <42601356+emitterpark@users.noreply.github.com>
2019-11-22 13:21:52 +0300

    do

--- arduino/lora-sensor/th-config/th-config.ino
+++ arduino/lora-sensor/th-config/th-config.ino
@@ -2,10 +2,10 @@
 
 struct Conf {
   const uint16_t read_t = 1;
-  const uint16_t send_t = 24;
+  const uint16_t send_t = 2;
   const float bat_lo_v = 3;       
-  const float alr_max[2] = {40, 65};
-  const float alr_min[2] = {10, 35};
+  const float alr_max[2] = {55, 85};
+  const float alr_min[2] = {5, 25};
   const float alr_hys[2] = {0.01, 0.01};    
 };
 
